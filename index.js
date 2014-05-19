97cba237d29ca318c232b7e1b34326da7af96dd0
figrrygg <gregers.gram.rygg@finn.no>
2014-05-19 14:07:09 +0200

    Resize container to rendered content size

---
+++ index.js
@@ -0,0 +1,11 @@
+function resize (gardrPluginApi){
+    gardrPluginApi.on('item:afterrender', function(item){
+        if (item.options.resizeHorizontal) {
+            var container = item.options.container;
+            container.style.width = item.rendered.width + 'px';
+            item.iframe.resize(item.rendered.width, item.iframe.height);
+        }
+    });
+}
+
+module.exports = resize;
